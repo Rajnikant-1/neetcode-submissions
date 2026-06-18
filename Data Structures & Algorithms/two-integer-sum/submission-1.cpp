4ce924f7dc72fabb42b36d6c54371bb568aceeeb
Rajnikant-1 <125971102+Rajnikant-1@users.noreply.github.com>
2026-06-19 04:50:22 +0530

    Update: two-integer-sum - submission-1

--- Data Structures & Algorithms/two-integer-sum/submission-1.cpp
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+
         unordered_map<int, int> mp;
-        for (int i= 0; i< nums.size(); i++){
+
+        for (int i = 0; i < nums.size(); i++) {
+
             int c = target - nums[i];
 
-            if (mp.find(c) != mp.end()){
-                return {mp[c],i};
+            // check presence of c
+            if (mp.find(c) != mp.end()) {
+                return {mp[c], i};
             }
-            mp[nums[i]]= i;
 
+            // store current number with index
+            mp[nums[i]] = i;
         }
+
+        return {};
     }
 };
